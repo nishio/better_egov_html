d7cec51310b745f2e1634dc40fd2760b239a592c
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2018-08-04 21:51:22 +0900

    add copy button

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 $(function(){
+  $("body").contentEditable = true;
+  $(".copy-button").click((e)=>{
+    var target = document.getElementById($(e.target).attr("target"));
+    $(target).css("background-color", "#ffd")
+    var range = document.createRange();
+    range.selectNode(target);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+  });
+
   $("#x2").click(()=>{
     $("body").css("width", "50%")
   })
   $("#x1").click(()=>{
     $("body").css("width", "100%")
   })
-
-  $("body").contentEditable = true;
-  $(".copy").click((e)=>{
-    var target = $(e.target).prev();
-    target.css("background-color", "#ffd")
-    var range = document.createRange();
-    range.selectNode(target[0]);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    document.execCommand('copy');
-  });
 });
